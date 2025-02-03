2747c542f541a96d77524d082affdccc1ee84385
Shubham Naik <shubhamnaik@Shubhams-Mac-mini.local>
2025-02-02 17:33:21 -0800

    chore: hackmerced 3,45

---
+++ src/IV/precache-manifest.c2deda573b641d707fbbde9815604385.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f057a199211ad186125",
+    "url": "/static/css/main.5b633af7.chunk.css"
+  },
+  {
+    "revision": "1f057a199211ad186125",
+    "url": "/static/js/main.450d5963.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ade595b08902c5d43b0",
+    "url": "/static/js/2.97605024.chunk.js"
+  },
+  {
+    "revision": "5ced842abe121b2d6d0020cc33bff716",
+    "url": "/static/media/raccoon.5ced842a.png"
+  },
+  {
+    "revision": "08da81b70c12b5ca067cf21a173488a8",
+    "url": "/static/media/CITRIS.08da81b7.png"
+  },
+  {
+    "revision": "700a1a0c6c8ecd6b95b13238b378ff77",
+    "url": "/static/media/UCANR.700a1a0c.png"
+  },
+  {
+    "revision": "02b217a7ccc061a20735adb27781fe3a",
+    "url": "/static/media/VentureLab.02b217a7.png"
+  },
+  {
+    "revision": "54e05d64cfba06861544f94d5405802f",
+    "url": "/static/media/Monster.54e05d64.png"
+  },
+  {
+    "revision": "c3097228841feab1623a3364d4421ef4",
+    "url": "/static/media/Wolfram.c3097228.png"
+  },
+  {
+    "revision": "a1c860c0d4de9c863c6394a56ee2ec4c",
+    "url": "/static/media/Vanguard.a1c860c0.png"
+  },
+  {
+    "revision": "849518ede5086006311d3b5a9b7ee3d1",
+    "url": "/static/media/Merco.849518ed.png"
+  },
+  {
+    "revision": "a6b02ec0492da740e05c73ff86e3e5ae",
+    "url": "/static/media/CZI.a6b02ec0.png"
+  },
+  {
+    "revision": "b79f66d48738bc64af034961711a359f",
+    "url": "/static/media/MakeyMakey.b79f66d4.png"
+  },
+  {
+    "revision": "a78d0eba67d040a243fb2cfb08370f33",
+    "url": "/static/media/YerbaMate.a78d0eba.png"
+  },
+  {
+    "revision": "86c47618b72a64a7f0077ce231ec84f7",
+    "url": "/static/media/CoolerMaster.86c47618.png"
+  },
+  {
+    "revision": "922efc215b38248d0904e485fb8d06e4",
+    "url": "/static/media/GoodDayChocolate.922efc21.png"
+  },
+  {
+    "revision": "54149675975705b862f8be5c096f7482",
+    "url": "/static/media/ASUCM.54149675.png"
+  },
+  {
+    "revision": "26b759b9644b9f16afd4cde1bd06cbe5",
+    "url": "/static/media/OSL.26b759b9.png"
+  },
+  {
+    "revision": "6a419d80110d15fda69237229da18116",
+    "url": "/static/media/MLH.6a419d80.png"
+  },
+  {
+    "revision": "dc2af56e0a30796f06d24ef1475ad5df",
+    "url": "/static/media/raccoon4.dc2af56e.png"
+  },
+  {
+    "revision": "97a4b6042392dad24fb26e83d4f73fc6",
+    "url": "/static/media/UCM.97a4b604.png"
+  },
+  {
+    "revision": "71c3d45a456c3cc57867d4c2d7cc52fc",
+    "url": "/index.html"
+  }
+];
