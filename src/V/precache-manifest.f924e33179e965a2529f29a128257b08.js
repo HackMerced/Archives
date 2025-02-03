6c2fccdac8b99ee8703d3fbe1388c135b8b19904
Shubham Naik <shubhamnaik@Shubhams-Mac-mini.local>
2025-02-02 17:33:34 -0800

    chore: hackmerced 3, 4, 5

---
+++ src/V/precache-manifest.f924e33179e965a2529f29a128257b08.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d31c6f6122fd1f600ac0f797976677b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08d0e65086e5be244c6d",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "897f3139d58eafec50dc",
+    "url": "/static/css/main.c7ce718f.chunk.css"
+  },
+  {
+    "revision": "08d0e65086e5be244c6d",
+    "url": "/static/js/2.dad5a3c2.chunk.js"
+  },
+  {
+    "revision": "897f3139d58eafec50dc",
+    "url": "/static/js/main.b37e2534.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dca0d06ed7622e339c4765e60b66b2e8",
+    "url": "/static/media/1password.dca0d06e.png"
+  },
+  {
+    "revision": "74be6603dffac88c4665132f2d160b1d",
+    "url": "/static/media/Background.74be6603.png"
+  },
+  {
+    "revision": "aa1b303897c11502700e10cf6188bdf6",
+    "url": "/static/media/Domain.com-logo.aa1b3038.png"
+  },
+  {
+    "revision": "5bbe98dde59e0b0c54a3fbb3d04e1316",
+    "url": "/static/media/Parking.5bbe98dd.jpg"
+  },
+  {
+    "revision": "d196619e8a383f288f5a03eaceac73ba",
+    "url": "/static/media/Rooms.d196619e.jpg"
+  },
+  {
+    "revision": "6a1bbd9768352c2501e6718b5a24ee09",
+    "url": "/static/media/asucm.6a1bbd97.png"
+  },
+  {
+    "revision": "436f6eff7242bb1273db6df15b7e8636",
+    "url": "/static/media/blockstack.436f6eff.png"
+  },
+  {
+    "revision": "be1fd4f41a89ede90b9030b07b6a13d7",
+    "url": "/static/media/citris.be1fd4f4.png"
+  },
+  {
+    "revision": "a00a6f6690cb904a5e21d8b025d36d36",
+    "url": "/static/media/cloudsploit.a00a6f66.png"
+  },
+  {
+    "revision": "323f2ba346ec16950ba1750d875659c7",
+    "url": "/static/media/desmos.323f2ba3.png"
+  },
+  {
+    "revision": "3b5888355968dbacbcba3677a9ecd74e",
+    "url": "/static/media/egghead.3b588835.png"
+  },
+  {
+    "revision": "e03793c003775378c6351c794fbf86bf",
+    "url": "/static/media/faq.e03793c0.png"
+  },
+  {
+    "revision": "eec79f6b6e454bf4e0403d81ed847bc9",
+    "url": "/static/media/film.eec79f6b.png"
+  },
+  {
+    "revision": "4de5099972f667c2841a3623e57bd0c1",
+    "url": "/static/media/founders.4de50999.png"
+  },
+  {
+    "revision": "64b94fed9e7afa4873774cd93506a7ce",
+    "url": "/static/media/github.64b94fed.png"
+  },
+  {
+    "revision": "e4484ea9a490c07cb1d7d53448315f41",
+    "url": "/static/media/googleCloud.e4484ea9.png"
+  },
+  {
+    "revision": "d04fe5baa6a150153500a306cc9c2d29",
+    "url": "/static/media/linode.d04fe5ba.png"
+  },
+  {
+    "revision": "39dd8f6cf3346a09d24fc68355b526af",
+    "url": "/static/media/llnl.39dd8f6c.png"
+  },
+  {
+    "revision": "afe718af6e690c3a2fb9af6a03fc1200",
+    "url": "/static/media/logo.afe718af.png"
+  },
+  {
+    "revision": "d786a92889cfad481b342d904b025126",
+    "url": "/static/media/makey makey.d786a928.png"
+  },
+  {
+    "revision": "fd322337e71d8b639a60bf2ff8d8ff43",
+    "url": "/static/media/medical.fd322337.png"
+  },
+  {
+    "revision": "6a419d80110d15fda69237229da18116",
+    "url": "/static/media/mlh-logo-white.6a419d80.png"
+  },
+  {
+    "revision": "6e0d4db55d4ea4c6aad5aff9a70b4a2d",
+    "url": "/static/media/mongodb.6e0d4db5.png"
+  },
+  {
+    "revision": "5309f9ea20641bf7f887aedfb04dadb6",
+    "url": "/static/media/radar.5309f9ea.png"
+  },
+  {
+    "revision": "298546b26205b3728e1f22043da36141",
+    "url": "/static/media/roche.298546b2.png"
+  },
+  {
+    "revision": "cce0d05ce55d2c00e8da37d4fba15e44",
+    "url": "/static/media/schedule.cce0d05c.png"
+  },
+  {
+    "revision": "807db34ad618e6926fac8d83e3fbe7cc",
+    "url": "/static/media/social good.807db34a.png"
+  },
+  {
+    "revision": "294e0815ccabdaaf6875000ff462f426",
+    "url": "/static/media/stickeryoulogo.294e0815.png"
+  },
+  {
+    "revision": "b1719a36f8b2ede2a480f90ee9c1adc4",
+    "url": "/static/media/sustainability.b1719a36.png"
+  },
+  {
+    "revision": "abf061a95a1dc2ce1fdafbc88b4a2fd6",
+    "url": "/static/media/uipath.abf061a9.png"
+  },
+  {
+    "revision": "1a00351d4a47f670ffde075ce1f3e5f7",
+    "url": "/static/media/wolfram.1a00351d.png"
+  }
+]);
